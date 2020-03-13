58c6d91a278774049c5d053392cdb39a99a98067
huynhtruongan <huhuhaha.not@gmail.com>
2020-03-13 16:31:13 +0700

    add handling error

--- JonasSchmedtmannCourses/starter/app.js
+++ JonasSchmedtmannCourses/starter/app.js
@@ -1,17 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const tourRouter = require('./routes/tourRouter');
+const tourRouter = require("./routes/tourRouter");
 //middleware
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
-	req.requestTime = new Date().toISOString();
-	next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 // app.use((req, res, next) => {
 // 	console.log(`Hello! i'm middleware`);
 // 	next();
 // });
-app.use('/api/v1/tours', tourRouter);
+app.use("/api/v1/tours", tourRouter);
 
+app.all("*", (req, res, next) => {
+  // res.status(404).json({
+  //   status: "fails",
+  //   message: `Can't find url ${req.originalUrl}, please check it again!!!`
+  // });
+  const err = new Error(
+    `Can't find url ${req.originalUrl}, please check it again!!!`
+  );
+  err.statusCode = 404;
+  err.status = "fails";
+
+  next(err);
+});
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+});
 module.exports = app;
--- JonasSchmedtmannCourses/starter/server.js
+++ JonasSchmedtmannCourses/starter/server.js
@@ -1,22 +1,25 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-dotenv.config({ path: './config.env' });
-const app = require('./app');
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
 mongoose
-	.connect(process.env.DATABASE_LOCAL, {
-		//.connect(DB,{
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useUnifiedTopology: true,
-		useFindAndModify: false
-	})
-	.then((con) => {
-		// console.log(con.connections);
-		console.log('DB connection successful!');
-	});
+  .connect(process.env.DATABASE_LOCAL, {
+    //.connect(DB,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(con => {
+    // console.log(con.connections);
+    console.log("DB connection successful!");
+  });
 
 // const testTour = new Tour({
 // 	name: 'The Park Camper',
@@ -33,5 +36,5 @@ mongoose
 // 	});
 // console.log(process.env);
 app.listen(process.env.PORT, () => {
-	console.log(`App running on port ${process.env.PORT}...`);
+  console.log(`App running on port ${process.env.PORT}...`);
 });
