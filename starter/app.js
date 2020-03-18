b981df5de146db4af64a99070942987a8ff5bec7
huynhtruongan <huhuhaha.not@gmail.com>
2020-03-18 22:30:01 +0700

    err handler

--- starter/app.js
+++ starter/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const globalErrorHandler = require("./controller/errorController");
 const tourRouter = require("./routes/tourRouter");
+const appError = require("./utils/appError");
 //middleware
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
@@ -19,21 +21,18 @@ app.all("*", (req, res, next) => {
   //   status: "fails",
   //   message: `Can't find url ${req.originalUrl}, please check it again!!!`
   // });
-  const err = new Error(
-    `Can't find url ${req.originalUrl}, please check it again!!!`
+  // const err = new Error(
+  //   `Can't find url ${req.originalUrl}, please check it again!!!`
+  // );
+  // err.statusCode = 404;
+  // err.status = "fails";
+
+  next(
+    new appError(
+      `Can't find url ${req.originalUrl}, please check it again!!!`,
+      404
+    )
   );
-  err.statusCode = 404;
-  err.status = "fails";
-
-  next(err);
 });
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler);
 module.exports = app;
