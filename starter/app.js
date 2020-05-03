a57fb86444babcae973cf55a0c3d052050790306
huynhtruongan <huhuhaha.not@gmail.com>
2020-05-03 22:02:26 +0700

    modeling data

--- starter/app.js
+++ starter/app.js
@@ -1,12 +1,55 @@
 const express = require("express");
-const app = express();
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+
+const appError = require("./utils/appError");
+
 const globalErrorHandler = require("./controller/errorController");
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
-const appError = require("./utils/appError");
-//middleware
-app.use(express.json());
+const app = express();
+
+// 1) Global middleware
+// Security HTTP headers
+app.use(helmet());
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!"
+});
+// Limit requets from same API
+app.use("/api", limiter);
+
+// Body parser, reading data from the body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+// Data sanitization again noSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsAverage",
+      "ratingQuatity",
+      "maxGroupSize",
+      "difficulty",
+      "price"
+    ]
+  })
+);
+
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
 
--- starter/models/tourModel.js
+++ starter/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const User = require("./userModel");
 const tourSchema = new mongoose.Schema(
   {
     name: {
@@ -8,84 +9,113 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
       maxlength: [40, "A tour name must have less or equal then 40 characters"],
-      minlength: [10, "A tour name must have more or equal then 10 characters"]
+      minlength: [10, "A tour name must have more or equal then 10 characters"],
     },
     slug: String,
     duration: {
       type: Number,
-      required: [true, "A tours must have a duration"]
+      required: [true, "A tours must have a duration"],
     },
     maxGroupSize: {
       type: Number,
-      required: [true, "A tour must have a group size"]
+      required: [true, "A tour must have a group size"],
     },
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty"],
       enum: {
         values: ["easy", "medium", "difficult"],
-        message: "Difficulty is either: easy, medium, difficult"
-      }
+        message: "Difficulty is either: easy, medium, difficult",
+      },
     },
     ratingsAverage: {
       type: Number,
       max: [5, "Rating must be below 5"],
       min: [1, "Rating must be abow 1"],
-      default: 4.5
+      default: 4.5,
     },
     ratingQuatity: {
       type: Number,
-      default: 0
+      default: 0,
     },
     price: {
       type: Number,
-      required: [true, "A tour must have a price"]
+      required: [true, "A tour must have a price"],
     },
     priceDiscount: {
       type: Number,
       validate: {
-        validator: function(val) {
+        validator: function (val) {
           //this only points to current doc on NEW document creation
           return val < this.price;
         },
-        message: "Discount price({VALUE}) should be below regular price"
-      }
+        message: "Discount price({VALUE}) should be below regular price",
+      },
     },
     summary: {
       type: String,
       trim: true,
-      required: [true, "A tour must have a summery"]
+      required: [true, "A tour must have a summery"],
     },
     description: {
       type: String,
-      trim: true
+      trim: true,
     },
     imageCover: {
       type: String,
-      required: [true, "A tour must have a cover image"]
+      required: [true, "A tour must have a cover image"],
     },
     images: [String],
     createdAt: {
       type: Date,
       default: Date.now(),
-      select: false
+      select: false,
     },
     secretTour: {
       type: Boolean,
-      default: false
+      default: false,
     },
-    startDates: [Date]
+    startDates: [Date],
+    startLocation: {
+      type: {
+        type: String,
+        default: "Point",
+        enum: ["Point"],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: "Point",
+          num: ["Point"],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
-    toObject: { virtuals: true }
+    toObject: { virtuals: true },
   }
 );
-tourSchema.virtual("durationWeeks").get(function() {
+tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
 });
 //DOCUMENT MIDDLEWARE: runs before .save() and .create()
-tourSchema.pre("save", function() {
+tourSchema.pre("save", function () {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
@@ -100,19 +130,24 @@ tourSchema.pre("save", function() {
 
 // QUERY MIDDLEWARE
 // tourSchema.pre('find', function(next) {
+tourSchema.pre("save", async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+  next();
+});
 
-tourSchema.pre(/^find/, function(next) {
+tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
   this.start = Date.now();
   next();
 });
-tourSchema.post(/^find/, function(docs, next) {
+tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   // console.log(docs);
   next();
 });
 // AGGREGATION MIDDLEWARE
-tourSchema.pre("aggregate", function(next) {
+tourSchema.pre("aggregate", function (next) {
   // console.log('pipeline', this.pipeline());
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
   next();
