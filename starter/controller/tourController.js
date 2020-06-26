8ba7e11a40c1d4506015c8af408eb51967c46a71
Nguyen Huynh Truong An <huhuhaha.not@gmail.com>
2020-06-26 16:15:49 +0700

    create reviewController

--- starter/controller/tourController.js
+++ starter/controller/tourController.js
@@ -1,9 +1,9 @@
 // const fs = require('fs');
-const Tour = require("./../models/tourModel");
-const APIFeatures = require("./../utils/apiFeatures");
+const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
-const catchAsync = require("./../utils/catchAsync");
-const AppError = require("./../utils/appError");
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 // const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
@@ -28,187 +28,183 @@ const AppError = require("./../utils/appError");
 // 	next();
 // };
 exports.aliasTopCheap = (req, res, next) => {
-  req.query.limit = 5;
-  req.query.sort = "price,-ratingsAverage";
-  req.query.fields = "name,price,ratingsAverage,difficulty,summary";
-  next();
+	req.query.limit = 5;
+	req.query.sort = 'price,-ratingsAverage';
+	req.query.fields = 'name,price,ratingsAverage,difficulty,summary';
+	next();
 };
 exports.getTourStats = catchAsync(async (req, res, next) => {
-  const stats = await Tour.aggregate([
-    {
-      $match: { ratingsAverage: { $gte: 4.5 } }
-    },
-    {
-      $group: {
-        _id: { $toUpper: "$difficulty" },
-        numTours: { $sum: 1 },
-        numRatings: { $sum: "$ratingsQuantity" },
-        avgRating: { $avg: "$ratingsAverage" },
-        avgPrice: { $avg: "$price" },
-        minPrice: { $min: "$price" },
-        maxPrice: { $max: "$price" }
-      }
-    },
-    {
-      $sort: { avgPrice: 1 }
-    }
-    // { $match: { _id: { $ne: 'EASY' } } }
-  ]);
-  res.status(200).json({
-    status: "success",
-    data: stats
-  });
+	const stats = await Tour.aggregate([
+		{
+			$match: { ratingsAverage: { $gte: 4.5 } }
+		},
+		{
+			$group: {
+				_id: { $toUpper: '$difficulty' },
+				numTours: { $sum: 1 },
+				numRatings: { $sum: '$ratingsQuantity' },
+				avgRating: { $avg: '$ratingsAverage' },
+				avgPrice: { $avg: '$price' },
+				minPrice: { $min: '$price' },
+				maxPrice: { $max: '$price' }
+			}
+		},
+		{
+			$sort: { avgPrice: 1 }
+		}
+		// { $match: { _id: { $ne: 'EASY' } } }
+	]);
+	res.status(200).json({
+		status: 'success',
+		data: stats
+	});
 });
 exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
-  const year = req.params.year * 1;
-  const plan = await Tour.aggregate([
-    {
-      $unwind: "$startDates"
-    },
-    {
-      $match: {
-        startDates: {
-          $gte: new Date(`${year}-01-01`),
-          $lte: new Date(`${year}-12-31`)
-        }
-      }
-    },
-    {
-      $group: {
-        _id: { $month: "$startDates" },
-        numTourStarts: { $sum: 1 },
-        tours: { $push: "$name" }
-      }
-    },
-    {
-      $addFields: { month: "$_id" }
-    },
-    {
-      $project: {
-        _id: 0
-      }
-    },
-    {
-      $sort: { month: 1 }
-    },
-    {
-      $limit: 12
-    }
-  ]);
-  res.status(200).json({
-    status: "success",
-    data: plan
-  });
+	const year = req.params.year * 1;
+	const plan = await Tour.aggregate([
+		{
+			$unwind: '$startDates'
+		},
+		{
+			$match: {
+				startDates: {
+					$gte: new Date(`${year}-01-01`),
+					$lte: new Date(`${year}-12-31`)
+				}
+			}
+		},
+		{
+			$group: {
+				_id: { $month: '$startDates' },
+				numTourStarts: { $sum: 1 },
+				tours: { $push: '$name' }
+			}
+		},
+		{
+			$addFields: { month: '$_id' }
+		},
+		{
+			$project: {
+				_id: 0
+			}
+		},
+		{
+			$sort: { month: 1 }
+		},
+		{
+			$limit: 12
+		}
+	]);
+	res.status(200).json({
+		status: 'success',
+		data: plan
+	});
 });
 exports.entireTours = catchAsync(async (req, res, next) => {
-  // console.log(req.requestTime);
+	// console.log(req.requestTime);
 
-  //BUID query
+	//BUID query
 
-  // EXECUTE query
-  const features = new APIFeatures(Tour, req.query)
-    .filter()
-    .sort()
-    .field()
-    .paginate();
-  const tour = await features.query;
-  // const tour = await Tour.find({'price[$gte]': '500' });
-  //query data
+	// EXECUTE query
+	const features = new APIFeatures(Tour, req.query).filter().sort().field().paginate();
+	const tour = await features.query;
+	// const tour = await Tour.find({'price[$gte]': '500' });
+	//query data
 
-  // const tour = await Tour.find({
-  // 	duration: 5,
-  // 	difficulty: 'easy'
-  // });
+	// const tour = await Tour.find({
+	// 	duration: 5,
+	// 	difficulty: 'easy'
+	// });
 
-  // const tour = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
-  // console.log(req.query);
-  //SEND response
-  res.status(200).json({
-    status: "success",
-    results: tour.length,
-    data: {
-      tour
-    }
-  });
+	// const tour = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
+	// console.log(req.query);
+	//SEND response
+	res.status(200).json({
+		status: 'success',
+		results: tour.length,
+		data: {
+			tour
+		}
+	});
 });
 exports.getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id);
-  if (!tour) {
-    return next(new AppError(`No tour found with that`, 404));
-  }
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour
-    }
-  });
+	const tour = await Tour.findById(req.params.id);
+	if (!tour) {
+		return next(new AppError(`No tour found with that`, 404));
+	}
+	res.status(200).json({
+		status: 'success',
+		data: {
+			tour
+		}
+	});
 
-  // const tour = tours.find((el) => el.id === id);
-  // res.status(200).json({
-  // 	status: 'success',
-  // 	results: tours.length,
-  // 	data: {
-  // 		tours: tour
-  // 	}
-  // });
+	// const tour = tours.find((el) => el.id === id);
+	// res.status(200).json({
+	// 	status: 'success',
+	// 	results: tours.length,
+	// 	data: {
+	// 		tours: tour
+	// 	}
+	// });
 });
 
 exports.postTour = catchAsync(async (req, res, next) => {
-  // const newId = tours[tours.length - 1].id + 1;
-  // const newTour = Object.assign({ id: newId }, req.body);
-  // tours.push(newTour);
-  // fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
-  // 	res.status(201).json({
-  // 		status: 'success',
-  // 		data: {
-  // 			tours: newTour
-  // 		}
-  // 	});
-  // });
-  // try {
-  const newTour = await Tour.create(req.body);
-  res.status(201).json({
-    status: "success",
-    data: {
-      tours: newTour
-    }
-  });
-  // } catch (err) {
-  //   res.status(404).json({
-  //     status: "fail",
-  //     message: err
-  //   });
-  // }
+	// const newId = tours[tours.length - 1].id + 1;
+	// const newTour = Object.assign({ id: newId }, req.body);
+	// tours.push(newTour);
+	// fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+	// 	res.status(201).json({
+	// 		status: 'success',
+	// 		data: {
+	// 			tours: newTour
+	// 		}
+	// 	});
+	// });
+	// try {
+	const newTour = await Tour.create(req.body);
+	res.status(201).json({
+		status: 'success',
+		data: {
+			tours: newTour
+		}
+	});
+	// } catch (err) {
+	//   res.status(404).json({
+	//     status: "fail",
+	//     message: err
+	//   });
+	// }
 });
 exports.updateTour = catchAsync(async (req, res, next) => {
-  // res.status(200).json({
-  // 	status: 'success',
-  // 	data: {
-  // 		tours: ' updated tours...'
-  // 	}
-  // });
+	// res.status(200).json({
+	// 	status: 'success',
+	// 	data: {
+	// 		tours: ' updated tours...'
+	// 	}
+	// });
 
-  const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true
-  });
-  if (!tour) {
-    return next(new AppError(`No tour found with that`, 404));
-  }
-  res.status(201).json({
-    status: "success",
-    data: {
-      tour
-    }
-  });
+	const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true
+	});
+	if (!tour) {
+		return next(new AppError(`No tour found with that`, 404));
+	}
+	res.status(201).json({
+		status: 'success',
+		data: {
+			tour
+		}
+	});
 });
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findByIdAndDelete(req.params.id);
-  if (!tour) {
-    return next(new AppError(`No tour found with that`, 404));
-  }
-  res.status(204).json({
-    status: "success",
-    data: null
-  });
+	const tour = await Tour.findByIdAndDelete(req.params.id);
+	if (!tour) {
+		return next(new AppError(`No tour found with that`, 404));
+	}
+	res.status(204).json({
+		status: 'success',
+		data: null
+	});
 });
--- starter/models/userModel.js
+++ starter/models/userModel.js
@@ -1,102 +1,94 @@
-const crypto = require("crypto");
-const mongoose = require("mongoose");
-const validator = require("validator");
-const bcrypt = require("bcryptjs");
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "User must have a name!"],
-    maxlength: [40, "User name must have less or equal then 40 characters"],
-    minlength: [10, "User name must have more or equal then 10 characters"]
-  },
-  email: {
-    type: String,
-    required: [true, "Please tell us your email"],
-    unique: true,
-    lowercase: true,
-    validate: [validator.isEmail, "Please provide a valid email"]
-  },
-  photo: String,
-  role: {
-    type: String,
-    enum: ["user", "guide", "lead-guide", "admin"],
-    default: "user"
-  },
-  password: {
-    type: String,
-    required: [true, "Please provide your password"],
-    minlength: 8,
-    select: false
-  },
-  passwordConfirm: {
-    type: String,
-    required: [true, "Please confirm your password"],
-    validate: {
-      // This only works on CREATE and SAVE !!!
-      validator: function(el) {
-        return el === this.password;
-      },
-      message: "password are not the same!"
-    }
-  },
-  passwordChangedAt: Date,
-  passwordResetToken: String,
-  passwordResetExpires: Date,
-  active: {
-    type: Boolean,
-    default: true,
-    select: false
-  }
+	name: {
+		type: String,
+		required: [ true, 'User must have a name!' ],
+		maxlength: [ 40, 'User name must have less or equal then 40 characters' ],
+		minlength: [ 10, 'User name must have more or equal then 10 characters' ]
+	},
+	email: {
+		type: String,
+		required: [ true, 'Please tell us your email' ],
+		unique: true,
+		lowercase: true,
+		validate: [ validator.isEmail, 'Please provide a valid email' ]
+	},
+	photo: String,
+	role: {
+		type: String,
+		enum: [ 'user', 'guide', 'lead-guide', 'admin' ],
+		default: 'user'
+	},
+	password: {
+		type: String,
+		required: [ true, 'Please provide your password' ],
+		minlength: 8,
+		select: false
+	},
+	passwordConfirm: {
+		type: String,
+		required: [ true, 'Please confirm your password' ],
+		validate: {
+			// This only works on CREATE and SAVE !!!
+			validator: function(el) {
+				return el === this.password;
+			},
+			message: 'password are not the same!'
+		}
+	},
+	passwordChangedAt: Date,
+	passwordResetToken: String,
+	passwordResetExpires: Date,
+	active: {
+		type: Boolean,
+		default: true,
+		select: false
+	}
 });
-userSchema.pre("save", async function(next) {
-  // Only run this function if password was actually modified
-  if (!this.isModified("password")) return next();
-  // Hash the password with cost of 12
+userSchema.pre('save', async function(next) {
+	// Only run this function if password was actually modified
+	if (!this.isModified('password')) return next();
+	// Hash the password with cost of 12
 
-  this.password = await bcrypt.hash(this.password, 12);
+	this.password = await bcrypt.hash(this.password, 12);
 
-  // Delete passwordConfirm field
-  this.passwordConfirm = undefined;
-  next();
+	// Delete passwordConfirm field
+	this.passwordConfirm = undefined;
+	next();
 });
 
-userSchema.pre("save", function(next) {
-  if (!this.isModified("password") || this.isNew) return next();
+userSchema.pre('save', function(next) {
+	if (!this.isModified('password') || this.isNew) return next();
 
-  this.passwordChangedAt = Date.now() - 1000;
-  next();
+	this.passwordChangedAt = Date.now() - 1000;
+	next();
 });
 userSchema.pre(/^find/, function(next) {
-  this.find({ active: { $ne: false } });
-  next();
+	this.find({ active: { $ne: false } });
+	next();
 });
-userSchema.methods.correctPassword = async function(
-  camdicatePassword,
-  userPassword
-) {
-  return await bcrypt.compare(camdicatePassword, userPassword);
+
+userSchema.methods.correctPassword = async function(camdicatePassword, userPassword) {
+	return await bcrypt.compare(camdicatePassword, userPassword);
 };
 userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
-  if (this.passwordChangedAt) {
-    const changedTimestamp = parseInt(
-      this.passwordChangedAt.getTime() / 1000,
-      10
-    );
-    // console.log(changedTimestamp, JWTTimestamp);
-    return JWTTimestamp < changedTimestamp;
-  }
+	if (this.passwordChangedAt) {
+		const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+		// console.log(changedTimestamp, JWTTimestamp);
+		return JWTTimestamp < changedTimestamp;
+	}
 };
 userSchema.methods.createPasswordResetToken = function() {
-  const resetToken = crypto.randomBytes(32).toString("hex");
+	const resetToken = crypto.randomBytes(32).toString('hex');
 
-  this.passwordResetToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
-  console.log({ resetToken }, this.passwordResetToken);
-  this.passwordResetExpires = Date.now() + 15 * 60 * 1000;
-  return resetToken;
+	this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+	console.log({ resetToken }, this.passwordResetToken);
+	this.passwordResetExpires = Date.now() + 15 * 60 * 1000;
+	return resetToken;
 };
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
