d4f22dc3a948bcefd09daf8f8c6346a48542df21
Nguyễn Huỳnh Trường An <35690285+huynhtruongan@users.noreply.github.com>
2020-08-30 22:13:28 +0700

    add 97

--- starter1/controllers/tourControllers.js
+++ starter1/controllers/tourControllers.js
@@ -37,10 +37,25 @@ exports.getAllTours = async (req, res) => {
 		let queryStr = JSON.stringify(queryObj);
 		queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-		console.log(JSON.parse(queryStr));
+		// console.log(JSON.parse(queryStr));
 
-		const query = Tour.find(JSON.parse(queryStr));
+		let query = Tour.find(JSON.parse(queryStr));
 
+		//3) Sorting
+		if (req.query.sort) {
+			const sortBy = req.query.sort.split(',').join(' ');
+			console.log(sortBy);
+			query = query.sort(sortBy);
+		} else {
+			query = query.sort('-createdAt');
+		}
+		// 4) Field limiting
+		if (req.query.fields) {
+			const fields = req.query.fields.split(',').join(' ');
+			query = query.select(fields);
+		} else {
+			query = query.select('-__v');
+		}
 		const tours = await query;
 
 		res.status(200).json({
--- starter1/models/tourModel.js
+++ starter1/models/tourModel.js
@@ -1,56 +1,57 @@
 const mongoose = require('mongoose');
 
 const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have name'],
-    unique: true,
-    trim: true,
-  },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have durations'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have max group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have difficulty'],
-  },
-  ratingAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuatity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trime: true,
-    required: [true, 'A tour must have a sumary'],
-  },
-  description: {
-    type: String,
-    trime: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  startDates: [Date],
+	name: {
+		type: String,
+		required: [ true, 'A tour must have name' ],
+		unique: true,
+		trim: true
+	},
+	duration: {
+		type: Number,
+		required: [ true, 'A tour must have durations' ]
+	},
+	maxGroupSize: {
+		type: Number,
+		required: [ true, 'A tour must have max group size' ]
+	},
+	difficulty: {
+		type: String,
+		required: [ true, 'A tour must have difficulty' ]
+	},
+	ratingAverage: {
+		type: Number,
+		default: 4.5
+	},
+	ratingsQuatity: {
+		type: Number,
+		default: 0
+	},
+	price: {
+		type: Number,
+		required: [ true, 'A tour must have a price' ]
+	},
+	priceDiscount: Number,
+	summary: {
+		type: String,
+		trime: true,
+		required: [ true, 'A tour must have a sumary' ]
+	},
+	description: {
+		type: String,
+		trime: true
+	},
+	imageCover: {
+		type: String,
+		required: [ true, 'A tour must have a cover image' ]
+	},
+	images: [ String ],
+	createdAt: {
+		type: Date,
+		default: Date.now(),
+		select: false
+	},
+	startDates: [ Date ]
 });
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
